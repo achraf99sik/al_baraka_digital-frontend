4a82033a081fc938f81b4f5b4c9e6b7c131809cd
Achraf Sikal <achrafsikal@gmail.com>
2026-01-12 13:03:03 +0100

    feat: define application routes for home, register, and login components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Home } from './module/home/home';
+import { Register } from './module/register/register';
+import { Login } from './module/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Home',
+    component: Home
+  },
+  {
+    path: 'register',
+    title: 'Register',
+    component: Register
+  },
+  {
+    path: 'login',
+    title: 'Login',
+    component: Login
+  }
+];
