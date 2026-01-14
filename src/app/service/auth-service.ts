061477589ee12d4759bf05625d0fa078f5a09321
Achraf Sikal <achrafsikal@gmail.com>
2026-01-14 15:44:30 +0100

    refactor: change refreshToken methode implementation to return the new access_token as an Observable stream and defining a boring logout methode

--- src/app/service/auth-service.ts
+++ src/app/service/auth-service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { CookieHelper } from '../helper/cookie-helper';
 import { ApiHelper } from '../helper/api-helper';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { LoginRequest } from '../dto/request/login-request';
 import { AuthResponse } from '../dto/responce/auth-response';
 import { RegisterRequest } from '../dto/request/register-request';
+import { map, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -38,13 +39,26 @@ export class AuthService extends ApiHelper {
       }
     );
   }
-  refreshToken() {
-    this.handleRequest(
-      this.http.post<AuthResponse>(`${this.api}/v1/auth/refresh-token`, null),
-      (res) => {
-        this.cookieHelper.set('access_token', res.access_token, 1);
-        this.cookieHelper.set('refresh_token', res.refresh_token, 7);
-      }
-    );
+  refreshToken(): Observable<string> {
+    const refreshToken = this.cookieHelper.get('refresh_token');
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${refreshToken}`,
+    });
+
+    return this.http
+      .post<AuthResponse>(`${this.api}/v1/auth/refresh-token`, null, { headers })
+      .pipe(
+        tap((res) => {
+          this.cookieHelper.set('access_token', res.access_token, 1);
+          this.cookieHelper.set('refresh_token', res.refresh_token, 7);
+        }),
+        map((res) => res.access_token)
+      );
+  }
+
+  logout() {
+    this.cookieHelper.destroy('refresh_token');
+    this.cookieHelper.destroy('access_token');
   }
 }
